fac62b48204a86d7e39c47f4d7e832fb619677b0
Darren Regan <darren111894@gmail.com>
2019-03-19 18:12:09 +0000

    Fixed HP couldn't see it in Unity
    Making class Seriallizable with [System.Serializable] made it so i could see player HP in unity

--- IT Project/Assets/__Scripts/Player.cs
+++ IT Project/Assets/__Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public class PlayerStats    {
+    [System.Serializable]
+    public class PlayerStats {
         public int Health = 100;
     }
 
