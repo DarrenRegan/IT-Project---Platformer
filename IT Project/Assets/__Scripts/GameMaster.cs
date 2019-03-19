99fe485261e0392c5802f279af3aec9b1509f54f
Darren Regan <darren111894@gmail.com>
2019-03-19 18:29:31 +0000

    Tags + Player Respawn
    Added a tag to GameMaster, working on  completing respawn for player

---
+++ IT Project/Assets/__Scripts/GameMaster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour{
+
+    //Creating a singleton so theres only access to a single instance of GM at one time
+    public static GameMaster gm;
+
+    //Transform for player respawning
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+
+    void Start(){
+        if (gm == null){
+            //gm = this;
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public void RespawnPlayer(){
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        //Maybe add spawn effect if i have enough time
+    }
+
+    //Pass in Player type from class player
+    public static void KillPlayer (Player player){
+        Destroy(player.gameObject); //Kills player
+    }
+
+
+
+}
