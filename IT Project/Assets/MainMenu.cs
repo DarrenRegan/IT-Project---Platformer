77f87fbc4d8a32528f61d203ea8288134558a74b
DarHallinan <G00342845@gmit.ie>
2019-03-26 15:10:31 +0000

    Added basic menu but weird errors are persisting

---
+++ IT Project/Assets/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start(){
+        
+    }
+    public void PlayGame (){
+        // loads up next scene in the queue
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
