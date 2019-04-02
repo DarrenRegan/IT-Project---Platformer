a4b9d624239fe89e19e3ab2296a249646f126643
Darren Regan <darren111894@gmail.com>
2019-04-02 21:53:27 +0100

    Finished Enemy HP Bar
    * Added more options to edit health in enemy script
    *Added a Header for Optional option
    * Added HP bar to enemies that decreases in size when hit
    * Added option to have HP bar decrease enabled or not since enemies that get oneshot might not need it

--- IT Project/Assets/__Scripts/Enemy.cs
+++ IT Project/Assets/__Scripts/Enemy.cs
@@ -4,20 +4,48 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-     [System.Serializable]
+    [System.Serializable]
     public class EnemyStats {
-        public int Health = 100;
+        //Variables
+        private int _curHealth;
+        public int maxHealth = 100;
+
+        public int curHealth{
+            get { return _curHealth; } //Returns private variable _curHealth
+            set { _curHealth = Mathf.Clamp(value, 0, maxHealth); } //Clamps HP between 0 and maxHealth to prevent negative HP values
+        }
+    
+        public void Init(){
+            curHealth = maxHealth; //Set hp at start of game
+        }
     }
 
     public EnemyStats enemyStats = new EnemyStats();
 
+    [Header("Optional: ")]
+    [SerializeField]
+    private Status status;
+
+    void Start(){
+        enemyStats.Init();
+
+        if(status != null){
+            status.SetHealth(enemyStats.curHealth, enemyStats.maxHealth);
+        }
+    }
+
     public void DamageEnemy (int damage){
 
-        enemyStats.Health -= damage;
+        enemyStats.curHealth -= damage;
 
-        if (enemyStats.Health <= 0){
+        if (enemyStats.curHealth <= 0){
             Debug.Log("Kill Player HP = 0");
             GameMaster.KillEnemy(this); //Kills current instance of enemy
         }
+
+        if(status != null){
+            status.SetHealth(enemyStats.curHealth, enemyStats.maxHealth);
+        }
+
     }
 }
---
+++ IT Project/Assets/__Scripts/Status.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Status : MonoBehaviour
+{
+    //Variables
+    [SerializeField] //Makes private variables show up in editor
+    private RectTransform healthBarRect;
+    [SerializeField]
+    private Text healthText;
+
+    void Start()
+    {
+        if (healthBarRect == null){
+            Debug.LogError("STATUS BAR: NO HP bar OBJECT REFERENCED");
+        }
+        if (healthText == null){
+            Debug.LogError("STATUS BAR: NO HP text OBJECT REFERENCED");
+        }
+    }//start()
+
+    public void SetHealth(int _cur, int _max)
+    {
+        float _value = ((float) _cur / _max);
+
+        healthBarRect.localScale = new Vector3(_value, healthBarRect.localScale.y, healthBarRect.localScale.z);
+        healthText.text = _cur + "/" + _max + " HP";
+    }
+}
